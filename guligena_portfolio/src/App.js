8b885c2cb3ecde3237c825aacaf1380465a59abe
GAierken <g.aierken@gmail.com>
2020-03-12 15:36:47 -0400

    added mp4 background

--- guligena_portfolio/src/App.js
+++ guligena_portfolio/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Route, Switch } from 'react-router'
+// import { Route, Switch } from 'react-router'
 import './App.css';
+import Mp4 from './Love-Coding.mp4'
 
 function App() {
+  console.log(Mp4)
   return (
     <div className="App">
-      <header className="App-header">
-         Guligena Aierken
-      </header>
+      <video autoPlay muted loop id="myVideo">
+        <source src={Mp4} type="video/mp4"/>
+      </video>
     </div>
   );
 }
