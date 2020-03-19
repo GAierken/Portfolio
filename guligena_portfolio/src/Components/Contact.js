6214f3dd90bb404e7db34903117406a519d7d2bc
GAierken <g.aierken@gmail.com>
2020-03-18 22:12:04 -0400

    added icon to contact.js

--- guligena_portfolio/src/Components/Contact.js
+++ guligena_portfolio/src/Components/Contact.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
-import { Grid, GridList } from '@material-ui/core'
-
+import { Grid, GridList, Container } from '@material-ui/core'
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import EmailIcon from '@material-ui/icons/Email';
 export default class Contact extends Component {
     render() {
         return (
-            <GridList className="contacts">
-                <Grid>ONe</Grid>
-            </GridList>
+            <React.Fragment>
+              <GridList >
+                <Grid><GitHubIcon style={{ fontSize: 60, position: 'fixed', color: '#ed6663', top: 300 }}  className="contacts"/></Grid>
+                <Grid><LinkedInIcon style={{ fontSize: 60, position: 'fixed', color: '#ed6663', top: 400 }} className="linkedin"/></Grid>
+                <Grid><EmailIcon style={{ fontSize: 60, position: 'fixed', color: '#ed6663', top: 500 }}  className="email"/></Grid>
+              </GridList>
+            </React.Fragment>
+            
         )
     }
 }
