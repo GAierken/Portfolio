0c07843a1bd0968d97a21a889fb7bf824888b3f9
GAierken <g.aierken@gmail.com>
2020-03-21 21:33:17 -0400

    added tooltip for links

--- guligena_portfolio/src/Components/Contact.js
+++ guligena_portfolio/src/Components/Contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Grid, GridList } from '@material-ui/core'
+import { Grid, GridList, Tooltip } from '@material-ui/core'
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import EmailIcon from '@material-ui/icons/Email';
@@ -17,9 +17,6 @@ export default class Contact extends Component {
         window.location.assign('https://www.linkedin.com/in/guligena-aierken/');
     }
 
-    handleFacebookClick = () => {
-        window.location.assign('https://www.facebook.com/people/G%C3%BClgin%C3%A4-%C3%84rkin/1253246231');
-    }
     
     handleMailClick = () => {
         window.location.assign('mailto:g.aierken@gmail.com')
@@ -39,11 +36,11 @@ export default class Contact extends Component {
         return (
             <React.Fragment>
               <GridList >
-                <Grid><GitHubIcon onClick={this.handleGitHubClick} style={{ fontSize: 23, position: 'fixed', color: '#ffffff', top: 0, left: 0 }}  className="contacts"/></Grid>
-                <Grid><LinkedInIcon onClick={this.handleLinkedInClick} style={{ fontSize: 25, position: 'fixed', color: '#ffffff', top: 0, left: 40 }} className="linkedin"/></Grid>
-                <Grid><DescriptionIcon onClick={this.handleDesClick} style={{ fontSize: 25, position: 'fixed', color: '#ffffff', top: 0, left: 80 }} className="linkedin"/></Grid>
-                <Grid><EmailIcon onClick={this.handleMailClick} style={{ fontSize: 25, position: 'fixed', color: '#ffffff', top: 0, left: 120 }}  className="email"/></Grid>
-                <Grid><AssignmentIndIcon  onClick={this.handleResumeClick} style={{ fontSize: 25, position: 'fixed', color: '#ffffff', top: 0, left: 160 }} /></Grid>
+                <Grid><Tooltip title="GitHub"><GitHubIcon onClick={this.handleGitHubClick} style={{ fontSize: 23, position: 'fixed', color: '#ffffff', top: 0, left: 0 }}  className="contacts"/></Tooltip></Grid>
+                <Grid><Tooltip title="LinkedIn"><LinkedInIcon onClick={this.handleLinkedInClick} style={{ fontSize: 25, position: 'fixed', color: '#ffffff', top: 0, left: 40 }} className="linkedin"/></Tooltip></Grid>
+                <Grid><Tooltip title="Blog"><DescriptionIcon onClick={this.handleDesClick} style={{ fontSize: 25, position: 'fixed', color: '#ffffff', top: 0, left: 80 }} className="linkedin"/></Tooltip></Grid>
+                <Grid><Tooltip title="Email"><EmailIcon onClick={this.handleMailClick} style={{ fontSize: 25, position: 'fixed', color: '#ffffff', top: 0, left: 120 }}  className="email"/></Tooltip></Grid>
+                <Grid><Tooltip title="Resume"><AssignmentIndIcon  onClick={this.handleResumeClick} style={{ fontSize: 25, position: 'fixed', color: '#ffffff', top: 0, left: 160 }} /></Tooltip></Grid>
               </GridList>
             </React.Fragment>
             
