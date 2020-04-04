06b5c8b6c9c570b402863ce0c1a03f9cacb94242
GAierken <g.aierken@gmail.com>
2020-04-03 23:39:15 -0400

    added menu on app.js

--- guligena_portfolio/src/App.js
+++ guligena_portfolio/src/App.js
@@ -11,14 +11,34 @@ import { Link as RouterLink } from 'react-router-dom';
 import PDFViewer from './Components/PDFViewer/PDFViewer';
 import PDFJSBackend from './backends/pdfjs.js'
 import { Typography } from '@material-ui/core';
-
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 class App extends React.Component {
  
+  state = {
+    menuClicked: false,
+    anchorEl: ''
+  }
 
- 
-  
- render() {
+ handleClick = (evt) => {
    
+   this.setState({
+     menuClicked: !this.state.menuClicked,
+     anchorEl: evt.currentTarget
+   })
+ }
+  
+ handleClose = () => {
+   this.setState({
+     menuClicked: !this.state.menuClicked,
+     anchorEl: null
+   })
+ }
+ 
+ render() {
+   console.log(this.state.menuClicked)
    
   return (
     <React.Fragment>
@@ -27,7 +47,29 @@ class App extends React.Component {
         <video autoPlay muted loop id="myVideo">
          <source src={Mp4} type="video/mp4"/>
         </video>
-        <Typography variant="h1">Guligena Aierken</Typography>
+        <Typography className="content name" align='center' variant="h1">Guligena Aierken</Typography>
+        <Typography className="content title" align='center' variant="h3">Full Stack Web Developer | Software Engineer</Typography>
+       
+          <IconButton 
+            className="menu icon button"
+            aria-label="more"
+            aria-haspopup="true"
+            onClick={this.handleClick}
+           >
+            <MoreVertIcon className='menu icon button'/>
+          </IconButton>
+          <Menu
+            open={this.state.menuClicked}
+            anchorEl={this.state.anchorEl}
+            keepMounted
+            onClose={this.handleClose}
+            >
+            <MenuItem>About</MenuItem>
+            <MenuItem>Projects</MenuItem>
+          </Menu>
+          
+
+
         <Contact/>
       </div>
     </React.Fragment>
